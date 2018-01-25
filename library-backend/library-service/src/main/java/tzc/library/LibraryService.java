4c419025564d2d4867237f710694c018a7f15e79
Lucian Tuca <tzclucian@gmail.com>
2018-01-25 14:58:38 +0200

    # Second session
    added application class

---
+++ library-backend/library-service/src/main/java/tzc/library/LibraryService.java
@@ -0,0 +1,11 @@
+package tzc.library;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Lucian Tuca
+ * @date 25/01/2018
+ */
+@SpringBootApplication
+public class LibraryService {
+}
